77c12ee9c5def0a946c0f899fefb9b5f05edfb1c
ultrawide <colinchan02@gmail.com>
2020-06-05 17:44:52 -0700

    Complete user sign-up functionality

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+/* Require controllers */
+var user_controller = require("../controllers/userController");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* GET sign-up page. */
-router.get('/sign-up', function(req, res, next) {
-  res.render('sign-up', {title: 'Sign-up' });
-});
+/// USER ROUTES ///
+
+/* GET request for signing up with a new account. */
+router.get("/sign-up", user_controller.sign_up_get);
 
 /* POST sign-up page. */
-router.post('/sign-up', function(req, res, next) {
-  res.render('sign-up', {title: 'Sign-up' });
-});
-
+router.post('/sign-up', user_controller.sign_up_post);
 
 module.exports = router;
